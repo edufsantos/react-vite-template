259ba16a29e72a768b929aee615131f13bb9a22f
Eduardo-Santos-HAPROCKET <eduardosantosifms@gmail.com>
2023-01-17 17:01:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { CardValidate } from "@solussaude-ui-teste/react_v1/dist";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <Example textValue="a" visible />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
